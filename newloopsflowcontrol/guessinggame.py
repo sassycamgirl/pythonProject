25ca6f448bb5372be4dc1ebe33f3ba526fcf9f52
sassycamgirl <dinnerfortwoo@gmail.com>
2022-07-07 14:37:34 -0500

    sending test file

--- newloopsflowcontrol/guessinggame.py
+++ newloopsflowcontrol/guessinggame.py
@@ -1,5 +1,28 @@
-
+"""
 python_release = 1994
 user_input = int(input('When was Python 1.0 released? '))
-while user_input != python_release:
+while user_input > python_release:
+    print('It was earlier than that!')
+    user_input = int(input('When was Python 1.0 released? '))
+    continue
+while user_input < python_release:
+    print('It was later than that!')
+    user_input = int(input('When was Python 1.0 released? '))
+    continue
+if user_input == python_release:
+    print('Correct!')
+"""
+
+answer = 0
+while True:
+    answer = int(input('When was Python 1.0 released? '))
+    if answer > 1994:
+        print('It was earlier than that!')
+    elif answer < 1994:
+        print('It was later than that!')
+    else:
+        print('Correct!')
+        break
+
+
 
