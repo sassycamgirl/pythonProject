b552511f7e60c0963d0af10872bc9727c1b85563
sassycamgirl <dinnerfortwoo@gmail.com>
2022-07-08 19:44:23 -0500

    update from work computer

---
+++ newlists/budgetexercise.py
@@ -0,0 +1,18 @@
+spendings = [1346.0, 987.50, 1734.40, 2567.0, 3271.45, 2500.0, 2130.0, 2510.30, 2987.34, 3120.50, 4069.78, 1000.0]
+low_spendings = []
+normal_spendings = []
+high_spendings = []
+
+for low in spendings:
+    if low < 1000:
+        low_spendings.append(low)
+
+for normal in spendings:
+    if (normal >= 1000) and (normal <= 2500):
+        normal_spendings.append(normal)
+
+for high in spendings:
+    if high > 2500:
+        high_spendings.append(high)
+print('Number of months with low spendings:{', len(low_spendings), '}, normal spendings:{', len(normal_spendings),
+      '}, high spendings:{', len(high_spendings), '}.', sep='')
