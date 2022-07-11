fa3caa857917b0c419e760308549acf07880bf0e
sassycamgirl <dinnerfortwoo@gmail.com>
2022-07-10 20:22:41 -0500

    work from main computer

---
+++ newlists/changingelementposition.py
@@ -0,0 +1,19 @@
+"""
+first = input('Enter first number: ')
+second = input('Enter second number: ')
+print('Before swapping:', first, second)
+temporary = first
+first = second
+second = temporary
+print('After Swapping:', first, second)
+"""
+
+"""
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+top_cities.sort()
+print(top_cities)
+"""
+
+random_numbers = [2, 5, 0, -3, 4]
+random_numbers.sort()
+print(random_numbers)
