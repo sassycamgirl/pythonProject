b7ac061ab37b4e13b2f2310e7fbbf3f3ba03a6b3
sassycamgirl <dinnerfortwoo@gmail.com>
2022-07-12 21:56:42 -0500

    work from main computer

---
+++ newlists/chkelementpresence.py
@@ -0,0 +1,11 @@
+"""
+for char in 'happy message':
+    print(char)
+"""
+
+invited_guests = ['Kate', 'Adam', 'Kerry', 'Joe', 'Anne', 'Marie']
+name = input('What is your name? ')
+if name in invited_guests:
+    print('Welcome!')
+else:
+    print('You are not invited!')
