f50998e44f1d66f6db1fdb31a78bd452f5e1a01c
sassycamgirl <dinnerfortwoo@gmail.com>
2022-07-15 21:43:40 -0500

    work from main computer

---
+++ newlists/tupleoperations.py
@@ -0,0 +1,17 @@
+user_data = ('John', 'American', 1964)
+print(len(user_data))
+
+if 'American' in user_data:
+    print('This person comes from the US!')
+
+for element in user_data:
+    print(element)
+
+user_datatwo = ('John', 'American', 1964) + ('Teacher', 'male')
+print(user_datatwo)
+
+numbers = (0, 1) * 10
+print(numbers)
+
+male_name = ['Adam', 'Jerry', 'Mark']
+berlin_temps = [ 13.0, 17.5, 12.0]
