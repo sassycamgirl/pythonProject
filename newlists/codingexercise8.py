4a6cdd9af815af27aa05c45a627fa6f6278eb995
sassycamgirl <dinnerfortwoo@gmail.com>
2022-07-19 22:24:20 -0500

    work from main computer

---
+++ newlists/codingexercise8.py
@@ -0,0 +1,18 @@
+"""
+might need for loop
+
+
+
+"""
+
+connections = [
+    ('Amsterdam', 'Dublin', 100),
+    ('Amsterdam', 'Rome', 140),
+    ('Rome', 'Warsaw', 130),
+    ('Minsk', 'Prague', 95),
+    ('Stockholm', 'Rome', 190),
+    ('Copenhagen', 'Paris', 120),
+    ('Madrid', 'Rome', 135),
+    ('Lisbon', 'Rome', 170),
+    ('Dublin', 'Rome', 170),
+    ]
