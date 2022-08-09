9f710eb34039390c2bdbf2b16bb070298ad86a38
sassycamgirl <dinnerfortwoo@gmail.com>
2022-08-08 21:10:14 -0500

    work from main computer

--- newlists/codingexercise9.py
+++ newlists/codingexercise9.py
@@ -6,5 +6,24 @@ sample_dict = {
     "face": "Gesicht",
     "nose": "Nase"
 }
+while True:
+    eng_input = input('Enter a word in English or EXIT: ')
+    print('Your answer is: ', eng_input, '\n'
+          'What your expecting: ', sample_dict['mouth'])
+    if eng_input == sample_dict:
+        print('this test: ', eng_input)
+        break
+    elif eng_input == 'EXIT':
+        print('Bye!')
+        break
 
-#eng_input
+
+
+"""
+if the_input = sample_dict
+    print(sample_dict.value)
+    else
+        print('Bye!'
+
+
+"""
