5676cf9d6ccc72bda9fba2006edf83a223ec112a
sassycamgirl <dinnerfortwoo@gmail.com>
2022-09-12 21:07:26 -0500

    work from main computer

---
+++ Functions-5/functionswparameters.py
@@ -0,0 +1,23 @@
+"""
+def get_average(input_numbers):
+    sum = 0.0
+    for number in input_numbers:
+        sum += number
+    average = sum / len(input_numbers)
+    print(average)
+
+get_average([5.0, 3.5, 7.8, 9.9, 10.0])
+"""
+
+def print_letter_count(text, letter):
+    counter = 0
+    for char in text:
+        if char == letter:
+            counter += 1
+    print('Number of', letter, 'is', counter)
+
+print_letter_count('Welcome', 'e')
+print_letter_count('People say nothing is impossible, but I do nothing every day', 'a')
+print_letter_count('e', 'Welcome')
+print_letter_count(text='Welcome', letter='e')
+print_letter_count(letter='e', text='Welcome')
