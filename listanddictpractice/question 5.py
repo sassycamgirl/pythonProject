b946ba1785dbe66b909a8503574f2f3e4820b12b
sassycamgirl <dinnerfortwoo@gmail.com>
2022-09-13 14:55:08 -0500

    update from work computer

--- listanddictpractice/question 5.py
+++ listanddictpractice/question 5.py
@@ -1,11 +1,9 @@
-values = [[3 - x for x in range(2)] for y in range(6)]
+values = [[1 - x for x in range(2)] for y in range(6)]
 print(values)
 
 sum = 0.0
 for row in values:
-    print(row)
     for cell in row:
-        print(cell)
         sum += cell
 
 print(sum)
