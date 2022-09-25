0bade077d50397c1842b4b1148e723fe132f12f7
sassycamgirl <dinnerfortwoo@gmail.com>
2022-09-25 17:23:15 -0500

    work from main computer

---
+++ Functions-5/nanmescopes.py
@@ -0,0 +1,10 @@
+def show_truth():
+    global mysterious_var
+    mysterious_var = "New Surpise!"
+    print(mysterious_var)
+
+mysterious_var = 'Surprise!'
+print(mysterious_var)
+show_truth()
+print(mysterious_var)
+
