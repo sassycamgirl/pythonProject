9931f4ca3d32b4eb5b94f31485e9aaea45bc0692
sassycamgirl <dinnerfortwoo@gmail.com>
2022-09-26 14:02:20 -0500

    update from work computer

---
+++ Functions-5/thenonevalue.py
@@ -0,0 +1,14 @@
+"""print_return = print('Hello world!')
+print(print_return)"""
+
+x = None
+if x is None:
+    print('yes')
+if x == None:
+    print('it does \n')
+
+def greet():
+    print('hello!')
+
+x = greet()
+print(x)
