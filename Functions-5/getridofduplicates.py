6a81108e7c7e7f8678620e189eb1122316afe014
sassycamgirl <dinnerfortwoo@gmail.com>
2022-09-26 19:32:35 -0500

    update from work computer

---
+++ Functions-5/getridofduplicates.py
@@ -0,0 +1,15 @@
+"""Write a function named unique. The function should accept one parameter, which is a list with any number of elements inside. The default value for the parameter should be an empty list ([]).
+
+The function should return a new list with all duplicate elements removed. The function should preserve the original order of elements.
+
+Example 1: for unique([1, 1, 4, 5, 1]), the output should be [1, 4, 5]
+Example 2: for unique(['Mark', 'Mark', 'John', 'Anne']), the output should be  ['Mark', 'John', 'Anne']"""
+
+"""
+def_unique([])
+    if statement
+    remove duplicate element
+    
+unique([1,1,4,5,1])
+unique(['Mark', 'Mark', 'John', 'Anne'])
+"""
