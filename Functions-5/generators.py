c75b72741e25c9198c3903510453cf96357629b9
sassycamgirl <dinnerfortwoo@gmail.com>
2022-10-14 19:57:53 -0500

    work from main computer

--- Functions-5/generators.py
+++ Functions-5/generators.py
@@ -1,3 +1,15 @@
 def get_number():
     for i in range(1, 4):
         yield i
+
+generator = get_number()
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print("\n")
+
+for x in get_number():
+    print(x)
+
+numbers = list(get_number())
+print("\n", numbers)
